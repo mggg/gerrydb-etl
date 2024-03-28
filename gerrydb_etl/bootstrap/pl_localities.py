5877d5fe82e9fb9376e0777ca1196958779c0a02
Chris Donnay <christopherdonnay@gmail.com>
2024-03-28 11:05:59 -0400

    make pl_init idempotent
    Add error handling to ensure that pl_init can be run even if some objects already exist in database

--- gerrydb_etl/bootstrap/pl_localities.py
+++ gerrydb_etl/bootstrap/pl_localities.py
@@ -6,8 +6,10 @@ from collections import Counter
 import click
 import geopandas as gpd
 import pandas as pd
-import us
+import us # package with tons of state metadata
+
 from gerrydb import GerryDB
+from gerrydb.exceptions import ResultError
 from gerrydb.schemas import LocalityCreate
 from gerrydb_etl import config_logger, download_dataframe_with_hash, pathify
 from utm import from_latlon
@@ -21,6 +23,7 @@ COUNTY_2020_URL = (
     "https://www2.census.gov/geo/tiger/TIGER2020/COUNTY/tl_2020_us_county.zip"
 )
 
+# __file__ is the absolute path of the pl_localities.py script
 NOTE_PREFIX = f"Loaded by ETL script {__file__} from the"
 STATE_NOTES = f"{NOTE_PREFIX} `us` package (https://github.com/unitedstates/python-us)"
 COUNTY_NOTES = (
@@ -72,7 +75,7 @@ ALIAS_OVERRIDES = {
     "51515": ["va/city-of-bedford", "virginia/bedford-city", "va/bedford-city"],
 }
 
-
+# utm is Universal Transverse Mercator, it's one of 60 zones used for projections
 def utm_of_point(point):
     """Returns the UTM zone of a lat-long point."""
     return from_latlon(point.y, point.x)[2]
@@ -86,7 +89,7 @@ def identify_utm_zone(df):
         utm_counts = Counter(utm_of_point(point) for point in df["geometry"].centroid)
     return utm_counts.most_common(1)[0][0]
 
-
+# EPSG European Petroleum Survey Group codes, used for identifying projection systems
 def utm_zone_proj(zone: int) -> str:
     """Returns an EPSG identifier for a zone-appropriate UTM projection."""
     if 3 <= zone <= 20:
@@ -104,64 +107,115 @@ def utm_zone_proj(zone: int) -> str:
     raise ValueError("Zone not covered by the U.S. Census.")
 
 
+# click is a library designed to create CLI (command line interface) programs
+# decorator, but nothing seems to be used here 
 @click.command()
-def load_localities():
+@click.option("--suppress_existence", 
+              is_flag = True, 
+              flag_value  = True,
+              help="Handle 'path already exists' errors.")
+def load_localities(suppress_existence: bool):
     """Imports localities for states, territories, and counties/county equivalents."""
+
+    # creates a gerrydb instance, which sets up communication between docker container DB and uvicorn web server
     db = GerryDB()
+
+    # Returns a (Geo)DataFrame and a file hash from a downloaded file
     counties_gdf, counties_hash = download_dataframe_with_hash(COUNTY_2020_URL)
 
     # Cross-vintage compatibility: prefer 2020 data, but add legacy counties
     # that were eliminated between 2010 and 2020.
+    # Returns a (Geo)DataFrame and a file hash from a downloaded file
     counties_2010_gdf, counties_2010_hash = download_dataframe_with_hash(
         COUNTY_2010_URL
     )
+
+    # remove 10 from the end of each column name
     counties_2010_gdf = counties_2010_gdf.rename(
         columns={
             col: col[:-2] if col.endswith("10") else col
             for col in counties_2010_gdf.columns
         }
     )
+
+    # counties removed between 2010 and 2020 census
     legacy_counties = set(counties_2010_gdf["GEOID"]) - set(counties_gdf["GEOID"])
     legacy_counties_gdf = counties_2010_gdf[
         counties_2010_gdf["GEOID"].isin(legacy_counties)
     ]
+
+    # create new geodataframe that includes 2020 and legacy counties
+    # TODO concat is being deprecated
     counties_gdf = gpd.GeoDataFrame(
         pd.concat([counties_gdf, legacy_counties_gdf], ignore_index=True),
         crs=counties_gdf.crs,
     )
 
+    # list of states, territories, and DC
     state_like = us.STATES_AND_TERRITORIES + [us.states.lookup("DC")]
+
+    # add state, territory, DC, and USA localities
+    # ctx is a WriteContext object, which stores all sorts of meta data about our transaction
     with db.context(notes=STATE_NOTES) as ctx:
         log.info("Creating root locality (us)...")
-        ctx.localities.create(
-            canonical_path="us",
-            name="United States of America",
-        )
+
+        # ctx.localities returns the LocalityRepo object associated to the GerryDB instance
+        # create method creates a locality, in this case the US
+        # will raise ResultError if path already exists
+        try:
+            ctx.localities.create(
+                canonical_path="us", # short identifier
+                name="United States of America", # full name 
+            )
+        except ResultError as e:
+            if suppress_existence and "Failed to create canonical path to new location(s)." in e.args[0]:
+                print("U.S. Path already exists.")
+                pass
+            else:
+                raise e
 
         state_like_locs = []
+
+        # for each state, territory, and DC
         for state in state_like:
             log.info("Creating locality for state/territory %s...", state.name)
-            state_gdf = counties_gdf[counties_gdf["STATEFP"] == state.fips]
+
+            # access all counties in state
+            state_gdf = counties_gdf[counties_gdf["STATEFP"] == state.fips] 
+
+            # identifies modal utm zone (locality could cross several)
             zone = identify_utm_zone(state_gdf)
+
+            # LocalityCreate stores info about the locality (but notably not the underlying geography!)
             state_like_locs.append(
                 LocalityCreate(
-                    canonical_path=pathify(state.name),
-                    parent_path="us",
-                    name=state.name,
-                    aliases=[state.fips, state.abbr.lower()],
-                    default_proj=utm_zone_proj(zone),
+                    canonical_path=pathify(state.name), #GerryPath constrained string
+                    parent_path="us", # GerryPath constrained string, probably denotes hierarchy
+                    name=state.name, #name of locality
+                    aliases=[state.fips, state.abbr.lower()], # aliases is a list of GerryPath constrained strings
+                    default_proj=utm_zone_proj(zone), # default geoegraphic projection
                 )
             )
 
         log.info(
             "Pushing localities for %d states/territories...", len(state_like_locs)
         )
-        ctx.localities.create_bulk(state_like_locs)
+        # create bulk creates localities in bulk from a list of LocalityCreate objects
+        # TODO unclear on how this actually ends up in the database
 
+        ctx.localities.create_bulk(state_like_locs)
+        
+
+
+    # us module has these built in under mapping method, but parker
+    # is being intentional about DC
     state_fips_to_name = {state.fips: state.name for state in state_like}
     state_fips_to_abbr = {state.fips: state.abbr for state in state_like}
 
+    # create a version of the dataframe without DC
     counties_gdf = counties_gdf[counties_gdf.STATEFP != "11"].copy()  # drop DC
+
+    # add new columns
     counties_gdf["state_name"] = counties_gdf["STATEFP"].map(state_fips_to_name)
     counties_gdf["state_abbr"] = counties_gdf["STATEFP"].map(state_fips_to_abbr)
     counties_gdf["full_name"] = (
@@ -169,6 +223,9 @@ def load_localities():
     )
     counties_gdf = counties_gdf.sort_values(by=["GEOID"])
 
+    # create context object to store meta data of transaction
+    # add counties
+    # TODO what are these hash things being stored?
     with db.context(
         notes=(
             COUNTY_NOTES
@@ -180,9 +237,16 @@ def load_localities():
         for row in counties_gdf.itertuples():
             log.info("Creating locality for %s...", row.full_name)
             zone = utm_of_point(row.geometry.centroid)
+
+            # if the row is in the override dict, use override value
+            # else use f"{pathify(row.state_name)}/{pathify(row.NAME)}"
             canonical_path = CANONICAL_PATH_OVERRIDES.get(
                 row.GEOID, f"{pathify(row.state_name)}/{pathify(row.NAME)}"
             )
+            # if the row is in the override dict, use override value
+            # else use f"{pathify(row.state_abbr)}/{pathify(row.NAME)}"
+
+            # adding the geoid and this path as aliases
             aliases = ALIAS_OVERRIDES.get(
                 row.GEOID, [f"{pathify(row.state_abbr)}/{pathify(row.NAME)}"]
             ) + [row.GEOID]
@@ -200,9 +264,12 @@ def load_localities():
         log.info(
             "Pushing localities for %d counties/county equivalents...", len(county_locs)
         )
+        
         ctx.localities.create_bulk(county_locs)
+        
 
 
+# meant to say this program is meant to be run, not part of library/package
 if __name__ == "__main__":
-    config_logger(log)
+    config_logger(log) # Configures a logger to write to `stderr`, from gerrydb_etl.__init__
     load_localities()
